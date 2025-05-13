e77a151fef7c86c8cb847bcbca8c2f4fdaf9fe50
윤서 <Yunse070708@gmail.com>
2025-05-14 00:32:32 +0900

    feat : 버튼 컴포넌트 퍼블리싱

---
+++ lib/components/move_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:harucourt/app/config/app_color.dart';
+import 'package:harucourt/app/config/app_text_styles.dart';
+
+class MoveButton extends StatelessWidget {
+  final String text;
+  const MoveButton({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 149),
+        decoration: BoxDecoration(
+          color: AppColor.primary1,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: AppColor.primary5, width: 1.0),
+        ),
+        child: Text(
+          "text",
+          style: AppTextStyles.btn1.copyWith(color: AppColor.primary5),
+        ),
+      ),
+    );
+  }
+}

d92c6c0e86074b0a17c4da6fba80ee5953aa92b1
윤서 <Yunse070708@gmail.com>
2025-05-19 11:13:53 +0900

    feat : GetX 경로 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:harucourt/screens/concernwriter.dart';
+import 'package:harucourt/screens/concern_writer.dart';
 import 'package:harucourt/screens/manual.dart';
 import 'package:harucourt/screens/onboarding.dart';
+import 'package:harucourt/screens/response_version.dart';
 
 void main() {
   runApp(const MainApp());
@@ -18,9 +19,10 @@ class MainApp extends StatelessWidget {
         GetPage(name: '/', page: () => OnboardingPage()),
         GetPage(name: '/Manual', page: () => ManualPage()),
         GetPage(name: '/ConcernWriter', page: () => ConcernWriterPage()),
+        GetPage(name: '/ResponseVersion', page: () => ResponseVersionPage()),
       ],
       // 실행 시 내가 원하는 페이지로 이동
-      initialRoute: '/ConcernWriter',
+      initialRoute: '/ResponseVersion',
     );
   }
 }
